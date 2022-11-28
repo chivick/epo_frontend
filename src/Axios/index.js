aa67ce02ce5e8357a6b3feb6950f088be3eeaf7a
Akalonu Victor Chimaobi <akalonuvictorchimaobi@gmail.com>
2022-11-28 13:33:34 +0100

    Changed the API endpoint

--- src/Axios/index.js
+++ src/Axios/index.js
@@ -4,7 +4,7 @@ import Axios from "axios";
 // const baseURL = "https://janapr.lonzecserver.com.ng";
 // const baseURL = "https://fbn.lonzecserver.com.ng";
  //const baseURL = "https://localhost:44376";
-const baseURL = "https://localhost:44389";
+const baseURL = "http://fbndisputeapp-dev.us-east-1.elasticbeanstalk.com";
 //const baseURL = "https://192.168.253.118:4444";
 //const baseURL = "http://192.168.253.118:4030";
 // const baseURL = "https://192.168.66.121:8000";
@@ -13,7 +13,8 @@ const baseURL = "https://localhost:44389";
 
 
 // const mayJunebaseURL = "https://mayjune.lonzecserver.com.ng";
-const mayJunebaseURL = "https://localhost:44389";
+// const mayJunebaseURL = "https://localhost:44389";
+const mayJunebaseURL = "http://fbndisputeapp-dev.us-east-1.elasticbeanstalk.com";
 //const mayJunebaseURL = "https://192.168.253.118:4444";
  //const mayJunebaseURL = "http://192.168.253.118:4030";
 //  const mayJunebaseURL = "https://fbn.lonzecserver.com.ng";
